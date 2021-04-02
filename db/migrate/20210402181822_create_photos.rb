ede52b0bfdb60bdb0457f3b458d79d68d8c6b91b
Jon Zivku <jon@minepi.com>
2021-04-02 12:10:44 -0700

    p4-1 completed

---
+++ db/migrate/20210402181822_create_photos.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class CreatePhotos < ActiveRecord::Migration[6.1]
+  def change
+    create_table :photos do |t|
+      t.datetime :date_time
+      t.string :file_name
+
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
