9b41ed3bf3966147de36347d5b8656850026960c
Jon Zivku <jon@minepi.com>
2021-04-02 18:04:59 -0700

    Added login & logout routes, and controller definitons

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,17 @@ class UsersController < ApplicationController
     @photos = Photo.where(user_id: params[:id])
     @user = User.find(params[:id])
   end
+
+  def login
+    # TODO login page
+  end
+  
+  def post_login
+    # TODO login action
+  end
+
+  def logout
+    # TODO logout logic
+    render :login
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get 'users/index'
   get 'photos/index/:id', to: 'users#show', as: 'user'
+
+  get 'users/login'
+  get 'users/post_login'
+  get '/users/logout'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
