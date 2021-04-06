d5776d21030c391740be5e36fa2d31aafbd118ae
Jon Zivku <jon@minepi.com>
2021-04-06 12:07:08 -0700

    reformatted routes for consistency

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'photos/index/:id', to: 'users#show', as: 'user'
+  get '/users/index'
+  get '/photos/index/:id', to: 'users#show', as: 'user'
 
-  get 'users/login'
-  get 'users/post_login'
-  get '/users/logout'
+  get '/users/login'
+  post '/users/post_login'
+  post '/users/logout'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
