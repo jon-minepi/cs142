c80ecfa34616c2b30d0d7c2a995dd3a1b1f1f94b
Jon Zivku <jon@minepi.com>
2021-04-07 13:54:27 -0700

    ok so now i understand what belongs_to is doing!

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,7 +15,7 @@ class CommentsController < ApplicationController
     respond_to do |format|
       if @comment.save
         format.html { redirect_to user_path(Photo.find(params[:id])[:user_id]) }
-        format.json { render :show, status: :created, location: @comment.parent_photo }
+        format.json { render :show, status: :created, location: @comment.photo }
       else
         puts @comment.errors.full_messages
         format.html { render :new, status: :unprocessable_entity }
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,13 +3,4 @@ class Comment < ApplicationRecord
   belongs_to :user
 
   validates :comment, :photo_id, :user_id, :date_time, presence: true
-
-  def parent_user
-    User.find(user_id)
-  end
-
-  def parent_photo
-    Photo.find(photo_id)
-  end
-
 end
