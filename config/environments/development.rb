859ab1171af6da0c63b01a6541487ae754217a29
Jon Zivku <jon@minepi.com>
2021-04-06 19:34:43 -0700

    whitelisted all ips for dev environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,5 +75,5 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
   #
   #
-  config.web_console.whitelisted_ips = '172.23.0.1'
+  config.web_console.whitelisted_ips = %w( 0.0.0.0/0 ::/0 )
 end
